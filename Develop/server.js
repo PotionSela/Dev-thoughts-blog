933a1f5410e563175440740067ce7cc48731b83f
Gisela <giselamata27@gmail.com>
2024-01-15 15:26:59 -0500

    Adding in sequelize and SequelizeStore

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,17 @@ const session = require('express-session');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
+const SeqelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: "Super secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
