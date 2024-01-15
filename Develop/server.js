83ffbc4903d84c037831e701d4223e4864fc4bdb
Gisela <giselamata27@gmail.com>
2024-01-15 15:29:30 -0500

    Adding in app.use

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
-const SeqelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,3 +17,14 @@ const sess = {
         db: sequelize
     })
 };
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(routes);
+
+sequelize.sync({ force: false}).then(() => {
+    app.listen(PORT, () => console.log('Now Listening!'));
+});
