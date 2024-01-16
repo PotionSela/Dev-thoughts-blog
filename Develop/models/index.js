49520b2c31590f6eae03901d011837d7aaa6781e
Gisela <giselamata27@gmail.com>
2024-01-16 18:17:53 -0500

    Adding in corrected code to foreign Key for post and comment

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -8,11 +8,11 @@ User.hasMany(Post, {
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'post_id'
+  foreignKey: 'user_id'
 });
 
 Comment.belongsTo(Post, {
-    foreignKey: 'comment_id'
+    foreignKey: 'post_id'
 });
 
 module.exports = { User, Post, Comment };
