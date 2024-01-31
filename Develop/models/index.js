d87d77472c203159ee070e90397618342cbe77b2
Gisela <giselamata27@gmail.com>
2024-01-31 13:32:35 -0500

    Setting up relationships between tables

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,18 +1,35 @@
+// Imports
 const User = require('./user');
 const Post = require('./post');
 const Comment = require('./comment');
 
+// Setting up relationships between the tables
 User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
 });
 
 Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
 });
 
 module.exports = { User, Post, Comment };
