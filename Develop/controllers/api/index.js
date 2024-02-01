464790ccb84f1fa6578bcd97df0730247967d334
Gisela <giselamata27@gmail.com>
2024-02-01 12:44:06 -0500

    Added proper routes

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,8 +1,13 @@
+// Imports
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
+// Middleware
 router.use('/userRoutes', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/postRoutes', postRoutes);
+router.use('/comment', commentRoutes);
 
+// Exports
 module.exports = router;
