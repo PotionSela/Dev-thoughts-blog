cd225dec0b55317f7c3cd5cec88510ea66f3a344
Gisela <giselamata27@gmail.com>
2024-02-01 11:58:14 -0500

    Forgot to add homeRoutes import and middleware

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,6 +1,12 @@
+// Imports
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require("./homeRoutes");
 
+//Middleware
+router.use("/", homeRoutes);
 router.use('/api', apiRoutes);
 
+
+// Export
 module.exports = router;
