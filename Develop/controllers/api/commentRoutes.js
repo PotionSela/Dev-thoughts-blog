d6c760e5568342b81e0b3fe9bb43906521c16d28
Gisela <giselamata27@gmail.com>
2024-02-01 13:48:38 -0500

    Added a delete route

--- Develop/controllers/api/commentRoutes.js
+++ Develop/controllers/api/commentRoutes.js
@@ -60,3 +60,22 @@ router.put("/:id", async (req, res) => {
 });
 
 // Delete a Comment
+router.delete("/:id", async (req, res) => {
+    try {
+        const comment = await Comment.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!comment) {
+            res.status(404).json({ message: "No comment found with that id!" });
+            return;
+        }
+        res.status(200).json(comment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Exports
+module.exports = router;
