f21a197fa63f9fbc4725b45b4c2ea2c75d0ec00c
Gisela <giselamata27@gmail.com>
2024-02-01 12:49:39 -0500

    Added router posts for new userData

---
+++ Develop/controllers/api/userRoutes.js
@@ -0,0 +1,19 @@
+// Imports
+const router = require("express").Router();
+const { User } = require("../../models");
+
+
+// Posts for new user, email, username, and password to database
+router.post("/", async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
