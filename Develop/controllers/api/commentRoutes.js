849b4dda297e8246caa9205ac9fd1fd2d89790cc
Gisela <giselamata27@gmail.com>
2024-02-01 13:25:54 -0500

    Adding a route to create a comment

--- Develop/controllers/api/commentRoutes.js
+++ Develop/controllers/api/commentRoutes.js
@@ -3,3 +3,17 @@ const router = require("express").Router();
 const { Post, Comment, User } = require("../../models");
 
 // Create a comment
+router.post("/", async (req, res) => {
+    try {
+        console.log("We have made it!");
+        const comment = await Comment.create({
+            comment_body: req.body.comment_body,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id || req.body.user_id,
+        });
+        res.status(200).json(comment);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
