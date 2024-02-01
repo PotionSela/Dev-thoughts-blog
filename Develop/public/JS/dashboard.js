e13b3e7096edf17bf2707a4de6c6e6c7e3fe6b7c
Gisela <giselamata27@gmail.com>
2024-02-01 15:06:59 -0500

    Adding functionality that allows for a user to edit the posts on dashboard

--- Develop/public/JS/dashboard.js
+++ Develop/public/JS/dashboard.js
@@ -16,3 +16,24 @@ const deletePostHandler = async (event) => {
         alert(response.statusText);
     }
 };
+
+// Function created that allows for a user to edit posts on dashboard by redirecting them to the /create/:id page
+const editPost = async (event) => {
+    event.preventDefault();
+    console.log("clicked button");
+
+    let postId = event.target.getAttribute("data-id");
+    document.location.assign(`/create/${postId}`);
+};
+const editButton = document.querySelectorAll("#editBtn");
+
+// Iterates over all buttons on the page and allows for edit functionality
+for (let i = 0; i < editButton.length; i++) {
+    editButton[i].addEventListener("click", editPost);
+}
+const deleteButton = document.querySelectorAll("#deleteBtn");
+
+// Iterates over all buttons on the page and allows for delete functionality
+for (let i = 0; i < deleteButton.length; i++) {
+    deleteButton[i].addEventListener("click", deletePostHandler);
+}
