0e85cb72a2d7de70866ebb8a3a2c9a3333fa4b70
Gisela <giselamata27@gmail.com>
2024-02-01 13:14:40 -0500

    Added route to edit an existing post

--- Develop/controllers/api/postRoutes.js
+++ Develop/controllers/api/postRoutes.js
@@ -17,3 +17,21 @@ router.post("/", withAuth, async (req, res) => {
     }
 });
 
+// A route to edit an existing post
+router.put("/:id", withAuth, async (req, res) => {
+    console.log(req.body);
+    try {
+        const postData = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!postData) {
+            res.status(404).json({ message: "There's no blog post found with this id!" });
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
