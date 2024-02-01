9a9f68af8dcde903ff51ca3ec39a589d828d7750
Gisela <giselamata27@gmail.com>
2024-02-01 11:54:26 -0500

    Added info for comment table model

--- Develop/models/comment.js
+++ Develop/models/comment.js
@@ -2,3 +2,50 @@
 const { Model, DataTypes} = require("sequelize");
 const sequelize = require("../config/connection");
 
+class Comment extends Model {};
+
+// Comment Table Model
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment_body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+        blogPost_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "blogPost",
+                key: "id",
+            },
+        },
+    },
+    {
+        // Link to database connection
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "comment",
+    }
+);
+
+// Export Link
+module.exports = Comment;
