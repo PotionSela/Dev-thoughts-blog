9c808566430b6fcf3356ab2a1dfb269aee303bb0
Gisela <giselamata27@gmail.com>
2024-02-01 13:10:33 -0500

    Added router post

--- Develop/controllers/api/postRoutes.js
+++ Develop/controllers/api/postRoutes.js
@@ -4,3 +4,16 @@ const { Post } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 // Route to create a new blog post
+router.post("/", withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newPost);
+    } catch (err) {
+        console.log(err)
+        res.status(400).json(err);
+    }
+});
+
